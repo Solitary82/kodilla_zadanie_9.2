5073d85617ae285bd8d7849661e95827bd8c1db0
Marta Waplak <mwaplak@gmail.com>
2018-02-02 20:40:16 +0100

    Modify .html, .css and .js files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+console.log(allNames);
+
+var newName = prompt('Please enter a name');
+
+if (allNames.indexOf(newName) === -1) {
+    allNames.push(newName);
+} else {
+    alert('This name is already entered. Please enter a different name.');
+}
